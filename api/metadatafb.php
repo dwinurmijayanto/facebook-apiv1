6cd665a857ca7095459127c8a15c5fb8e5376b91
dwi nur mijayanto <sangpenguasa@gmail.com>
2026-01-19 15:15:42 +0700

    Create metadatafb.php

---
+++ api/metadatafb.php
@@ -0,0 +1,9 @@
+{
+    "status": "success",
+    "data": {
+        "judul": "Facebook",
+        "thumbnail": "https://scontent-sin11-1.xx.fbcdn.net/v/t15.5256-10/595453826_33171922399072784_7520028684391515271_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=a27664&_nc_ohc=CLeNYk5oHTYQ7kNvwFrDNR5&_nc_oc=Adkd58yypUnIOx6Go3bVKQDMX0eUvXGBkMdoypbzBv4A5uFD_TsXG_qGv2k1kzBAWOo&_nc_zt=23&_nc_ht=scontent-sin11-1.xx&_nc_gid=nunb8V_K8xk4g03S-Gathg&oh=00_AfpeKHbDmA-RLn-mcKr1WEcJGX66UFe6DGOQvyEJCRk5Tg&oe=6973ABFD",
+        "download": "https://video-sin6-2.xx.fbcdn.net/o1/v/t2/f2/m412/AQMPEjmIxY9s05_6DO6tF4sUa3ys9qy7d8OZj0Ok4Hu8sbAS5jBmm-MQSGN_GJfoaPo8rFkgKmLEy4irzp6CkyeW0IR78fIKy_P-uDhdnA.mp4?_nc_cat=109&_nc_sid=8bf8fe&_nc_ht=video-sin6-2.xx.fbcdn.net&_nc_ohc=42DHEF0wvH8Q7kNvwE2NzXF&efg=eyJ2ZW5jb2RlX3RhZyI6Inhwdl9wcm9ncmVzc2l2ZS5GQUNFQk9PSy4uQzMuMzYwLnN2ZV9zZCIsInhwdl9hc3NldF9pZCI6ODMzNjQwMzEyNzU2NjQ4LCJhc3NldF9hZ2VfZGF5cyI6NDQsInZpX3VzZWNhc2VfaWQiOjEwMTIyLCJkdXJhdGlvbl9zIjo4Mzk2LCJ1cmxnZW5fc291cmNlIjoid3d3In0%3D&ccb=17-1&_nc_gid=AUzpY6ojGP1WaafCxj9v2w&_nc_zt=28&oh=00_AfoKPxiCtuJ0jHg6vaUCJWGMtTaevMFUCw4RU323Cke5EQ&oe=6973BBA1&bitrate=322598&tag=sve_sd",
+        "durasi": "02:19:56"
+    }
+}
